304f411c021880856581dd3fd7d69bdd56dbd44d
devsifat <devsifat@gmail.com>
2020-11-23 00:28:08 +0900

    commented nav to check why deploy does not work

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
 
     return (
         <nav id="navbar" class="navbar navbar-light bg-light fixed-top ">
-            <a class="navbar-brand" href="#" rel="noreferrer">#SIFII</a>
+            {/* <a class="navbar-brand" href="#" >#SIFII</a>
             <ul class="nav nav-pills">
                 <li class="nav-item">
                     <a class="nav-link" href="#about_me">About Me</a>
@@ -30,13 +30,13 @@ function Navbar() {
                         aria-haspopup="true" aria-expanded="false">CV | Resume</a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href={state.resume} target="_blank">Resume</a>
-                        {/* <a class="dropdown-item" href="academic_CV_md_sifatul_islam.pdf" target="_blank">CV (Academic)</a> */}
-                        {/* <a class="dropdown-item" href="sifatul_resume.pdf" target="_blank">Resume </a> */}
+                        <a class="dropdown-item" href="academic_CV_md_sifatul_islam.pdf" target="_blank">CV (Academic)</a>
+                        <a class="dropdown-item" href="sifatul_resume.pdf" target="_blank">Resume </a>
                         <div role="separator" class="dropdown-divider"></div>
                          <a class="dropdown-item"    href={state.resume_korean} target="_blank">이럭서</a>
                     </div>
                 </li>
-            </ul>
+            </ul> */}
         </nav>
     );
 }
