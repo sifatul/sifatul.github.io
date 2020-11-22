af754dd902017b02f4ddcd6e2d38cf2ebb6d76e0
devsifat <devsifat@gmail.com>
2020-11-23 00:41:06 +0900

    CI made false

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { getBasicInfo } from "./lib/Firebase";
 import React from 'react';
 function Navbar() {
-    const [state, setstate] = React.useState({});
-    console.log(state)
+    const [state, setstate] = React.useState({})
     
     React.useEffect(() => {
         try {
@@ -14,7 +14,7 @@ function Navbar() {
 
     return (
         <nav id="navbar" class="navbar navbar-light bg-light fixed-top ">
-            {/* <a class="navbar-brand" href="#" >#SIFII</a>
+            <a class="navbar-brand" href="#">#SIFII</a>
             <ul class="nav nav-pills">
                 <li class="nav-item">
                     <a class="nav-link" href="#about_me">About Me</a>
@@ -30,14 +30,12 @@ function Navbar() {
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
                         aria-haspopup="true" aria-expanded="false">CV | Resume</a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href={state.resume} target="_blank">Resume</a>
-                        <a class="dropdown-item" href="academic_CV_md_sifatul_islam.pdf" target="_blank">CV (Academic)</a>
-                        <a class="dropdown-item" href="sifatul_resume.pdf" target="_blank">Resume </a>
+                        <a class="dropdown-item" href={state.resume} target="_blank">Resume</a> 
                         <div role="separator" class="dropdown-divider"></div>
                          <a class="dropdown-item"    href={state.resume_korean} target="_blank">이럭서</a>
                     </div>
                 </li>
-            </ul> */}
+            </ul>
         </nav>
     );
 }
