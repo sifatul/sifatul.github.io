07277decddb837cfabac87a972cb212ce225c088
sifatul-teraark <76417921+sifatul-teraark@users.noreply.github.com>
2020-12-26 14:58:21 +0900

    GA added

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 function Footer (){
     return            ( <div class=" row footer ">
-    <p class="m-auto py-3"><i class="far fa-envelope"></i> devsifat@gmail.com.ac.kr</p>
+    <p class="m-auto py-3"><i class="far fa-envelope"></i> devsifat@gmail.com</p>
 </div>);
 
 }
